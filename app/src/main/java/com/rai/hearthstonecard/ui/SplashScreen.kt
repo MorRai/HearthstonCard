3b412ded6e9fb200715f2c796a5100007855820b
Denis Zabolotski <morsmes@gmail.com>
2022-07-03 23:00:02 +0300

    6 path

---
+++ app/src/main/java/com/rai/hearthstonecard/ui/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.rai.hearthstonecard.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.rai.hearthstonecard.databinding.ActivitySplashBinding
+
+
+
+class SplashScreen : AppCompatActivity()  {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        with(binding){
+            imgBackgroud.animate().translationY(-2500.0f).setDuration(1000).startDelay = 2000
+            appMade.animate().translationY(2000.0f).setDuration(1000).startDelay = 2000
+            animationView.animate().translationY(1500.0f).setDuration(1000).startDelay=2000
+        }
+
+
+
+        Handler().postDelayed({ startActivity(Intent(this@SplashScreen, MainActivity::class.java)) },
+            3000)
+    }
+}
