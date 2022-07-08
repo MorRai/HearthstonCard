527c94b11294f33bce87be6fdac71a9c840d4d6a
Denis Zabolotski <morsmes@gmail.com>
2022-07-08 21:01:05 +0300

    6 path

--- app/src/main/java/com/rai/hearthstonecard/ui/SplashScreen.kt
+++ app/src/main/java/com/rai/hearthstonecard/ui/SplashScreen.kt
@@ -1,35 +1,49 @@
 package com.rai.hearthstonecard.ui
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.rai.hearthstonecard.R
 import com.rai.hearthstonecard.databinding.ActivitySplashBinding
 
 
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : Fragment() {
 
-class SplashScreen : AppCompatActivity()  {
-
-    private lateinit var binding: ActivitySplashBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        with(binding){
-            imgBackgroud.animate().translationY(-2500.0f).setDuration(1000).startDelay = 2000
-            appMade.animate().translationY(2000.0f).setDuration(1000).startDelay = 2000
-            animationView.animate().translationY(1500.0f).setDuration(1000).startDelay=2000
+    private var _binding: ActivitySplashBinding? = null
+    private val binding
+        get() = requireNotNull(_binding) {
+            "View was destroyed"
         }
 
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ActivitySplashBinding.inflate(inflater, container, false)
+            .also { binding ->
+                _binding = binding
+            }
+            .root
+    }
 
-        Handler().postDelayed({ startActivity(Intent(this@SplashScreen, MainActivity::class.java)) },
-            3000)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            imgBackgroud.animate().translationY(-2500.0f).setDuration(1000).startDelay = 2000
+            appMade.animate().translationY(2000.0f).setDuration(1000).startDelay = 2000
+            animationView.animate().translationY(1500.0f).setDuration(1000).startDelay = 2000
+        }
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_contentFragment)
+        }, 3000)
     }
 }
